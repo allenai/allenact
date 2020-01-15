4bcb9addfdb499bd63d4b7198d8055567369db59
Jordi Salvador <jordis@allenai.org>
2020-01-14 17:52:40 -0800

    Specialized A2C_ACKTR for cleaner configuration

--- a2c_ppo_acktr/algo/__init__.py
+++ a2c_ppo_acktr/algo/__init__.py
@@ -1,2 +1,48 @@
 from .a2c_acktr import A2C_ACKTR
 from .ppo import PPO
+
+
+class A2C(A2C_ACKTR):
+    def __init__(
+        self,
+        actor_critic,
+        value_loss_coef,
+        entropy_coef,
+        lr=None,
+        eps=None,
+        alpha=None,
+        max_grad_norm=None,
+    ):
+        super().__init__(
+            actor_critic,
+            value_loss_coef,
+            entropy_coef,
+            lr,
+            eps,
+            alpha,
+            max_grad_norm,
+            acktr=False,
+        )
+
+
+class ACKTR(A2C_ACKTR):
+    def __init__(
+        self,
+        actor_critic,
+        value_loss_coef,
+        entropy_coef,
+        lr=None,
+        eps=None,
+        alpha=None,
+        max_grad_norm=None,
+    ):
+        super().__init__(
+            actor_critic,
+            value_loss_coef,
+            entropy_coef,
+            lr,
+            eps,
+            alpha,
+            max_grad_norm,
+            acktr=True,
+        )
