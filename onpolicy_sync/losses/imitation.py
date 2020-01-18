8835da631f1f04ccfe36eb535eed90ab8b88526a
lucaw <lucaw@allenai.org>
2020-01-17 16:39:40 -0800

    Some more updates to docs and imitation (also deleted some dead code).

--- onpolicy_sync/losses/imitation.py
+++ onpolicy_sync/losses/imitation.py
@@ -16,17 +16,35 @@ class Imitation(AbstractActorCriticLoss):
         **kwargs
     ):
         if "expert_action" in batch["observations"]:
-            expert_actions = batch["observations"]["expert_action"]
-            total_loss = -(
-                actor_critic_output.distributions.log_probs(expert_actions)
-            ).mean()
-        elif "expert_policy" in batch["observations"]:
-            expert_policies = batch["observations"]["expert_policy"]
-            total_loss = (
-                -(actor_critic_output.distributions.log_probs_tensor * expert_policies)
-                .sum(-1)
-                .mean()
+            expert_actions_and_mask = batch["observations"]["expert_action"]
+            assert expert_actions_and_mask.shape[-1] == 2
+            expert_actions_and_mask_reshaped = expert_actions_and_mask.view(-1, 2)
+
+            expert_actions = expert_actions_and_mask_reshaped[:, 0].view(
+                *expert_actions_and_mask.shape[:-1], 1
             )
+            expert_actions_masks = (
+                expert_actions_and_mask_reshaped[:, 1]
+                .float()
+                .view(*expert_actions_and_mask.shape[:-1], 1)
+            )
+
+            expert_successes = expert_actions_masks.sum()
+            if expert_successes.item() == 0:
+                return 0, {}
+
+            total_loss = -(
+                expert_actions_masks
+                * actor_critic_output.distributions.log_probs(expert_actions)
+            ).sum() / torch.clamp(expert_successes, min=1)
+        elif "expert_policy" in batch["observations"]:
+            raise NotImplementedError()
+            # expert_policies = batch["observations"]["expert_policy"]
+            # total_loss = (
+            #     -(actor_critic_output.distributions.log_probs_tensor * expert_policies)
+            #     .sum(-1)
+            #     .mean()
+            # )
         else:
             raise NotImplementedError(
                 "Imitation loss requires either `expert_action` or `expert_policy`"
--- rl_base/sensor.py
+++ rl_base/sensor.py
@@ -37,15 +37,22 @@ class Sensor(Generic[EnvType]):
         self.observation_space = self._get_observation_space()
 
     def _get_uuid(self, *args: Any, **kwargs: Any) -> str:
+        """The unique ID of the sensor.
+        @param args: extra args.
+        @param kwargs: extra kwargs.
+        """
         raise NotImplementedError()
 
     def _get_observation_space(self) -> gym.Space:
+        """The observation space of the sensor.
+        """
         raise NotImplementedError()
 
     def get_observation(
         self, env: EnvType, task: "Task", *args: Any, **kwargs: Any
     ) -> Any:
-        """
+        """Returns observations from the environment (or task).
+
         @param env: the environment the sensor is used upon.
         @param task: (optionally) a Task from which the sensor should get data.
         @return: current observation for Sensor.
@@ -66,6 +73,9 @@ class SensorSuite(Generic[EnvType]):
     observation_spaces: SpaceDict
 
     def __init__(self, sensors: List[Sensor]) -> None:
+        """
+        @param sensors: the sensors that will be included in the suite.
+        """
         self.sensors = OrderedDict()
         spaces: OrderedDict[str, gym.Space] = OrderedDict()
         for sensor in sensors:
@@ -77,6 +87,10 @@ class SensorSuite(Generic[EnvType]):
         self.observation_spaces = SpaceDict(spaces=spaces)
 
     def get(self, uuid: str) -> Sensor:
+        """Return sensor with the given `uuid`.
+        @param uuid: the unique id of the sensor
+        @return: the sensor with unique id `uuid`.
+        """
         return self.sensors[uuid]
 
     def get_observations(
@@ -89,3 +103,35 @@ class SensorSuite(Generic[EnvType]):
             uuid: sensor.get_observation(env=env, task=task, *args, **kwargs)
             for uuid, sensor in self.sensors.items()
         }
+
+
+class ExpertActionSensor(Sensor[EnvType]):
+    def __init__(self, config: Dict[str, Any], *args: Any, **kwargs: Any) -> None:
+        super().__init__(config, *args, **kwargs)
+        self.config = config
+        self.uuid = self._get_uuid()
+        self.observation_space = self._get_observation_space()
+
+    def _get_uuid(self, *args: Any, **kwargs: Any) -> str:
+        return "expert_action"
+
+    def _get_observation_space(self) -> gym.spaces.Tuple:
+        """The observation space of the expert action sensor. Will equal
+            `gym.spaces.Tuple(gym.spaces.Discrete(num actions in task), gym.spaces.Discrete(2))`
+            where the first entry of the tuple is the expert action index and the second equals
+            0 if and only if the expert failed to generate a true expert action. The value
+            `num actions in task` should be in `config["nactions"]`
+        """
+        return gym.spaces.Tuple(
+            (gym.spaces.Discrete(self.config["nactions"]), gym.spaces.Discrete(2))
+        )
+
+    def get_observation(
+        self, env: EnvType, task: "Task", *args: Any, **kwargs: Any
+    ) -> Any:
+        action, expert_was_successful = task.query_expert()
+        assert isinstance(action, int), (
+            "In expert action sensor, `task.query_expert()` "
+            "did not return an integer action."
+        )
+        return action, int(expert_was_successful)
