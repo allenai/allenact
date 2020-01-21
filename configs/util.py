1857545fbc67e43d5c0166f0770ba514eb9b1cdb
Jordi Salvador <jordis@allenai.org>
2020-01-21 11:50:29 -0800

    Recursive kwargs update in Builder

--- configs/util.py
+++ configs/util.py
@@ -1,5 +1,6 @@
-import copy
 from typing import NamedTuple, Dict, Any, Type
+import copy
+import collections.abc
 
 
 class Builder(NamedTuple):
@@ -7,8 +8,16 @@ class Builder(NamedTuple):
     kwargs: Dict[str, Any] = {}
     default: Dict[str, Any] = {}
 
+    def update(self, d, u):
+        for k, v in u.items():
+            if isinstance(v, collections.abc.Mapping):
+                d[k] = self.update(d.get(k, {}), v)
+            else:
+                d[k] = v
+        return d
+
     def __call__(self, **kwargs):
         allkwargs = copy.deepcopy(self.default)
-        allkwargs.update(self.kwargs)
-        allkwargs.update(kwargs)
+        self.update(allkwargs, self.kwargs)
+        self.update(allkwargs, kwargs)
         return self.class_name(**allkwargs)
