0887b46bf8872ffd5551175557db37e7b686dd1b
Jordi Salvador <jordis@allenai.org>
2020-01-22 12:47:55 -0800

    Cleaner config selection in main

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from onpolicy_sync.arguments import get_args
 from onpolicy_sync.trainer import Trainer
+import inspect
 import importlib
 
 
 def load_config(args):
-    module, configclass = args.experiment_config_class.rsplit(".", 1)
-    module = importlib.import_module(".".join([args.experiment_base, module]))
-    return getattr(module, configclass)()
+    module_path = "{}.{}".format(args.experiment_base, args.experiment)
+    module = importlib.import_module(module_path)
+
+    experiments = [
+        m[1]
+        for m in inspect.getmembers(module, inspect.isclass)
+        if m[1].__module__ == module.__name__
+    ]
+    assert (
+        len(experiments) == 1
+    ), "Too many or two few experiments defined in {}".format(module_path)
+
+    return experiments[0]()
 
 
 def main():
