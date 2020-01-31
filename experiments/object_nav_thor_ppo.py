388691b7bce2198b32a849b8cab8404ee6f16ff9
lucaw <lucaw@allenai.org>
2020-01-31 14:09:59 -0800

    Splitting our example experiment into two different experiments.

--- experiments/object_nav_thor_ppo.py
+++ experiments/object_nav_thor_ppo.py
@@ -7,33 +7,35 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from onpolicy_sync.losses.ppo import PPOConfig
 from models.object_nav_models import ObjectNavBaselineActorCritic
+from onpolicy_sync.losses import PPO
+from onpolicy_sync.losses.ppo import PPOConfig
 from rl_ai2thor.ai2thor_sensors import RGBSensorThor, GoalObjectTypeThorSensor
 from rl_ai2thor.object_nav.task_samplers import ObjectNavTaskSampler
 from rl_ai2thor.object_nav.tasks import ObjectNavTask
-from utils.experiment_utils import LinearDecay, Builder, PipelineStage, TrainingPipeline
-from onpolicy_sync.losses import PPO
-from onpolicy_sync.losses.imitation import Imitation
 from rl_base.experiment_config import ExperimentConfig
-from rl_base.sensor import SensorSuite, ExpertActionSensor
+from rl_base.sensor import SensorSuite
 from rl_base.task import TaskSampler
+from utils.experiment_utils import Builder, PipelineStage, TrainingPipeline
 
 
-class ObjectNavThorExperimentConfig(ExperimentConfig):
-    """An object navigation experiment in THOR."""
+class ObjectNavThorPPOExperimentConfig(ExperimentConfig):
+    """An object navigation experiment in THOR.
 
-    # OBJECT_TYPES = sorted(["Cup", "Television", "Tomato"])
+    Training with PPO.
+    """
+
+    # Easy setting
     OBJECT_TYPES = sorted(["Tomato"])
-    TRAIN_SCENES = [
-        "FloorPlan1_physics"
-    ]  # ["FloorPlan{}".format(i) for i in range(1, 21)]
-    VALID_SCENES = [
-        "FloorPlan1_physics"
-    ]  # ["FloorPlan{}_physics".format(i) for i in range(21, 26)]
-    TEST_SCENES = [
-        "FloorPlan1_physics"
-    ]  # ["FloorPlan{}_physics".format(i) for i in range(26, 31)]
+    TRAIN_SCENES = ["FloorPlan1_physics"]
+    VALID_SCENES = ["FloorPlan1_physics"]
+    TEST_SCENES = ["FloorPlan1_physics"]
+
+    # Hard setting
+    # OBJECT_TYPES = sorted(["Cup", "Television", "Tomato"])
+    # TRAIN_SCENES = ["FloorPlan{}".format(i) for i in range(1, 21)]
+    # VALID_SCENES = ["FloorPlan{}_physics".format(i) for i in range(21, 26)]
+    # TEST_SCENES = ["FloorPlan{}_physics".format(i) for i in range(26, 31)]
 
     SCREEN_SIZE = 224
 
@@ -46,7 +48,6 @@ class ObjectNavThorExperimentConfig(ExperimentConfig):
             }
         ),
         GoalObjectTypeThorSensor({"object_types": OBJECT_TYPES}),
-        ExpertActionSensor({"nactions": 6}),
     ]
 
     ENV_ARGS = {
@@ -65,19 +66,17 @@ class ObjectNavThorExperimentConfig(ExperimentConfig):
 
     @classmethod
     def tag(cls):
-        return "ObjectNav"
+        return "ObjectNavPPO"
 
     @classmethod
     def training_pipeline(cls, **kwargs):
-        dagger_steps = int(3e4)
-        ppo_steps = int(3e4)
-        ppo_steps2 = int(1e6)
+        ppo_steps = int(1e6)
         lr = 2.5e-4
-        num_mini_batch = 1
+        num_mini_batch = 1 if not torch.cuda.is_available() else 6
         update_repeats = 3
-        num_steps = 16
-        log_interval = 2 * num_steps * cls.machine_params("train")["nprocesses"]
-        save_interval = 2 * log_interval
+        num_steps = 128
+        log_interval = cls.MAX_STEPS * 10  # Log every 10 max length tasks
+        save_interval = 10000  # Save every 10000 steps (approximately)
         gamma = 0.99
         use_gae = True
         gae_lambda = 1.0
@@ -89,33 +88,21 @@ class ObjectNavThorExperimentConfig(ExperimentConfig):
             num_mini_batch=num_mini_batch,
             update_repeats=update_repeats,
             num_steps=num_steps,
-            named_losses={
-                "imitation_loss": Builder(Imitation,),
-                "ppo_loss": Builder(PPO, default=PPOConfig,),
-            },
+            named_losses={"ppo_loss": Builder(PPO, default=PPOConfig,),},
             gamma=gamma,
             use_gae=use_gae,
             gae_lambda=gae_lambda,
             max_grad_norm=max_grad_norm,
             pipeline_stages=[
-                PipelineStage(
-                    loss_names=["imitation_loss"],
-                    teacher_forcing=LinearDecay(
-                        startp=1.0, endp=0.0, steps=dagger_steps,
-                    ),
-                    end_criterion=dagger_steps,
-                ),
-                PipelineStage(
-                    loss_names=["ppo_loss", "imitation_loss"], end_criterion=ppo_steps
-                ),
-                PipelineStage(loss_names=["ppo_loss"], end_criterion=ppo_steps2,),
+                PipelineStage(loss_names=["ppo_loss"], end_criterion=ppo_steps,),
             ],
         )
 
     @classmethod
     def machine_params(cls, mode="train", **kwargs):
+        print("Is cuda available? {}".format(torch.cuda.is_available()))
         if mode == "train":
-            nprocesses = 1
+            nprocesses = 6 if not torch.cuda.is_available() else 30
             gpu_ids = [] if not torch.cuda.is_available() else [0]
         elif mode == "valid":
             nprocesses = 0
