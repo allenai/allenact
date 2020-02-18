e3b3799ef6dadca6880b0f11ac22b89ce73b57cc
klemenkotar <klemenk@allenai.org>
2020-02-18 01:00:26 -0800

    Fixed bug inmaking fake 3 channel input for resent

--- rl_habitat/habitat_sensors.py
+++ rl_habitat/habitat_sensors.py
@@ -329,7 +329,8 @@ class DepthResNetSensorHabitat(Sensor[HabitatEnvironment, PointNavTask]):
             depth = np.array(self.scaler(self.to_pil(depth)), dtype=np.float32)
 
         depth = self.to_tensor(depth).squeeze()
-        depth = torch.stack([3 * depth], dim=2).unsqueeze(0)
+        depth = torch.stack([depth] * 3, dim=0).unsqueeze(0)
+
         if torch.cuda.is_available():
             depth = depth.to("cuda:0")
         depth = self.resnet(depth).detach().cpu().numpy()
