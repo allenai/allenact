2d98079142c99bbb0d0c46bc1902dfb28c86630a
klemenkotar <klemenk@allenai.org>
2020-02-27 13:26:09 -0800

    Switched to using reshape instead of view in flatten

--- models/basic_models.py
+++ models/basic_models.py
@@ -19,7 +19,7 @@ class Flatten(nn.Module):
         # Returns
         Flattened tensor.
         """
-        return x.view(x.size(0), -1)
+        return x.reshape(x.size(0), -1)
 
 
 class SimpleCNN(nn.Module):
