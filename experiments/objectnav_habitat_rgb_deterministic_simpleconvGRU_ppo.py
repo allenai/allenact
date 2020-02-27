76cac784bcf00c52b20b6849c03a22773fbe0089
klemenkotar <klemenk@allenai.org>
2020-02-27 02:19:59 -0800

    Added proper action space for objectnav

--- experiments/objectnav_habitat_rgb_deterministic_simpleconvGRU_ppo.py
+++ experiments/objectnav_habitat_rgb_deterministic_simpleconvGRU_ppo.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 from models.object_nav_models import ObjectNavBaselineActorCritic
 from rl_base.sensor import SensorSuite
-from rl_habitat.habitat_tasks import PointNavTask
+from rl_habitat.habitat_tasks import ObjectNavTask
 from rl_habitat.habitat_sensors import RGBSensorHabitat, TargetObjectSensorHabitat
 from rl_habitat.habitat_utils import construct_env_configs
 from experiments.objectnav_habitat_base import ObjectNavHabitatBaseExperimentConfig
@@ -38,7 +38,7 @@ class ObjectNavHabitatRGBeterministicSimpleConvGRUPPOExperimentConfig(ObjectNavH
     @classmethod
     def create_model(cls, **kwargs) -> nn.Module:
         return ObjectNavBaselineActorCritic(
-            action_space=gym.spaces.Discrete(len(PointNavTask.action_names())),
+            action_space=gym.spaces.Discrete(len(ObjectNavTask.action_names())),
             observation_space=SensorSuite(cls.SENSORS).observation_spaces,
             goal_sensor_uuid="target_object_id",
             hidden_size=512,
