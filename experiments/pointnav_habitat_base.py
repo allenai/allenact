063e8c699cb61e8dd5e3cbc1292bc9e85ee5a394
klemenkotar <klemenk@allenai.org>
2020-03-01 17:41:03 -0800

    Added experiment with training the last few layers of resnet

--- experiments/pointnav_habitat_base.py
+++ experiments/pointnav_habitat_base.py
@@ -33,7 +33,7 @@ class PointNavHabitatBaseExperimentConfig(ExperimentConfig):
     MAX_STEPS = 500
     DISTANCE_TO_GOAL = 0.2
 
-    NUM_PROCESSES = 6
+    NUM_PROCESSES = 32
 
     SENSORS = [
         RGBSensorHabitat(
@@ -70,7 +70,7 @@ class PointNavHabitatBaseExperimentConfig(ExperimentConfig):
     CONFIG = habitat.get_config('configs/gibson.yaml')
     CONFIG.defrost()
     CONFIG.NUM_PROCESSES = NUM_PROCESSES
-    CONFIG.SIMULATOR_GPU_IDS = [0]
+    CONFIG.SIMULATOR_GPU_IDS = [1,2,3,4,5,6,7]
     CONFIG.DATASET.SCENES_DIR = 'habitat/habitat-api/data/scene_datasets/'
     CONFIG.DATASET.POINTNAVV1.CONTENT_SCENES = ['*']
     CONFIG.DATASET.DATA_PATH = TRAIN_SCENES
