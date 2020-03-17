996678ca47c3abcf1826b1f7f73e891d5f1a73ce
klemenkotar <klemenk@allenai.org>
2020-03-17 00:40:08 -0700

    Switched tyo logging whether we are pasuing procs or not

--- onpolicy_sync/vector_sampled_tasks.py
+++ onpolicy_sync/vector_sampled_tasks.py
@@ -416,7 +416,6 @@ class VectorSampledTasks(object):
         List of outputs from the step method of tasks.
         """
         self.async_step(actions)
-        print("Paused:", self._paused)
         return self.wait_step()
 
     def reset_all(self):
@@ -483,6 +482,7 @@ class VectorSampledTasks(object):
         write_fn = self._connection_write_fns.pop(index)
         worker = self._workers.pop(index)
         self._paused.append((index, read_fn, write_fn, worker))
+        print("PAUSING!")
 
     def resume_all(self) -> None:
         """Resumes any paused processes."""
