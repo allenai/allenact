0c4cdaae81f8a2fd06ec1322283e7949febb0a38
klemenkotar <klemenk@allenai.org>
2020-04-01 03:41:49 -0700

    Fixed config file for ddppo

--- experiments/pointnav_habitat_base.py
+++ experiments/pointnav_habitat_base.py
@@ -33,7 +33,7 @@ class PointNavHabitatBaseExperimentConfig(ExperimentConfig):
     MAX_STEPS = 500
     DISTANCE_TO_GOAL = 0.2
 
-    NUM_PROCESSES = 12
+    NUM_PROCESSES = [12, 12, 12, 12, 12, 12, 12, 12] # 12
 
     SENSORS = [
         RGBSensorHabitat(
@@ -70,7 +70,7 @@ class PointNavHabitatBaseExperimentConfig(ExperimentConfig):
     CONFIG = habitat.get_config('configs/gibson.yaml')
     CONFIG.defrost()
     CONFIG.NUM_PROCESSES = NUM_PROCESSES
-    CONFIG.SIMULATOR_GPU_IDS = [1, 2, 3, 4, 5, 6]
+    CONFIG.SIMULATOR_GPU_IDS = [0, 1, 2, 3, 4, 5, 6, 7]
     CONFIG.DATASET.SCENES_DIR = 'habitat/habitat-api/data/scene_datasets/'
     CONFIG.DATASET.POINTNAVV1.CONTENT_SCENES = ['*']
     CONFIG.DATASET.DATA_PATH = TRAIN_SCENES
@@ -146,14 +146,14 @@ class PointNavHabitatBaseExperimentConfig(ExperimentConfig):
             gpu_ids = [] if not torch.cuda.is_available() else [0, 1, 2, 3, 4, 5, 6, 7]
             render_video = False
         elif mode == "valid":
-            nprocesses = 1
+            nprocesses = [1]
             if not torch.cuda.is_available():
                 gpu_ids = []
             else:
                 gpu_ids = [7]
             render_video = False
         elif mode == "test":
-            nprocesses = 12
+            nprocesses = [1]
             if not torch.cuda.is_available():
                 gpu_ids = []
             else:
