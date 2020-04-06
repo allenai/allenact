2efa8a53d1ca0f11de3aa814cc02f3cb9cb4fa69
klemenkotar <klemenk@allenai.org>
2020-04-06 12:17:19 -0700

    Removed runner debvugging

--- onpolicy_sync/runner.py
+++ onpolicy_sync/runner.py
@@ -400,7 +400,6 @@ class OnPolicyRunner(object):
                     collected.append(package)
                     if len(collected) >= nworkers:
                         collected = sorted(collected, key=lambda x: x[2])  # sort by num_steps
-                        print("COLLECTED! Length:", len(collected), "\n\n\n\n\n\n\n", collected, "\n\n\n\n\n\n\n")
                         if collected[nworkers - 1][2] == collected[0][2]:  # ensure nworkers have provided the same num_steps
                             last_train_steps, last_train_time = self.process_train_packages(
                                 log_writer,
@@ -408,7 +407,7 @@ class OnPolicyRunner(object):
                                 last_steps=last_train_steps,
                                 last_time=last_train_time,
                             )
-                            collected = [] # TODO Revert this, this is very bad --> collected[nworkers:]
+                            collected = collected[nworkers:]
                         elif len(collected) > 2 * nworkers:
                             raise Exception("Unable to aggregate train packages from {} workers".format(nworkers))
                 elif package[0] == "valid_package":
