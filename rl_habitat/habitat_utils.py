54ecd2d9939f7c0f7a3979d89b133fcadd12cac1
klemenkotar <klemenk@allenai.org>
2020-04-13 14:27:43 -0700

    Set up run with 32 prcos for aws machine

--- rl_habitat/habitat_utils.py
+++ rl_habitat/habitat_utils.py
@@ -25,8 +25,7 @@ def construct_env_configs(config: Config) -> List[Config]:
               'jh4fc5c5qoQ', 'YmJkqBEsHnH', 'i5noydFURQK', 'cV4RVeZvu5T', 'aayBHfsNo7d', 'pRbA3pwrgk9', '82sE5b5pLXE',
               'S9hNv5qa7GM', 'sT4fr6TAbpF', 'b8cTxDM8gDG', 'D7N2EKCX4Sj', 'Uxmj2M2itWa', '17DRP5sb8fy', 'JeFG25nYj2p',
               '8WUmhLawc2A', 'SN83YJsR3w2', 'XcA2TqTSSAj', '5q7pvUzZiYa', '759xd9YjKW5', 's8pcmisQ38h', 'sKLMLpTHeUy',
-              'qoiz87JEwZ2', 'uNb9QFRL6hY', 'e9zR4mvMWw7', 'rPc6DW4iMge', 'vyrNrziPKCB', 'V2XKFyX4ASd', 'VFuaQ6m2Qom',
-              'ZMojNkEp431', '5LpN3gDmAk7', 'r47D5H71a5s', 'ULsKaCPVFJR', 'E9uDoFAP3SH']
+              'qoiz87JEwZ2', 'uNb9QFRL6hY', 'e9zR4mvMWw7', 'rPc6DW4iMge']
 
     if len(scenes) > 0:
         random.shuffle(scenes)
