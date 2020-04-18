05c381bd138dbff83c19469f9cc52f0099e3e02b
klemenkotar <klemenk@allenai.org>
2020-04-18 16:38:23 -0700

    Increased val frequency to 1M

--- experiments/objectnav_habitat_base_ddppo.py
+++ experiments/objectnav_habitat_base_ddppo.py
@@ -74,7 +74,7 @@ class ObjectNavHabitatDDPPOBaseExperimentConfig(ExperimentConfig):
         num_mini_batch = 1
         update_repeats = 4
         num_steps = 30
-        save_interval = 5000000
+        save_interval = 1000000
         log_interval = 10000
         gamma = 0.99
         use_gae = True
