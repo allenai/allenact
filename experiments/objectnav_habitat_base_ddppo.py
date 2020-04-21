f8f128837cc7afe6a865d31eba835b49e938aed5
klemenkotar <klemenk@allenai.org>
2020-04-21 10:47:16 -0700

    Added exeprimentconfig for 1 scene run

--- experiments/objectnav_habitat_base_ddppo.py
+++ experiments/objectnav_habitat_base_ddppo.py
@@ -25,8 +25,8 @@ class ObjectNavHabitatDDPPOBaseExperimentConfig(ExperimentConfig):
     TRAIN_SCENES = "habitat/habitat-api/data/datasets/objectnav/mp3d/v0/train_chair/train.json.gz"
     VALID_SCENES = "habitat/habitat-api/data/datasets/objectnav/mp3d/v0/val_chair/val.json.gz"
 
-    TRAIN_SCENES = "habitat/habitat-api/data/datasets/objectnav/mp3d/v0/val_chair/val_ten_tasks_ten_scenes.json.gz"
-    VALID_SCENES = "habitat/habitat-api/data/datasets/objectnav/mp3d/v0/val_chair/val_ten_tasks_ten_scenes.json.gz"
+    TRAIN_SCENES = "habitat/habitat-api/data/datasets/objectnav/mp3d/v0/val_chair/val_one_scene.json.gz"
+    VALID_SCENES = "habitat/habitat-api/data/datasets/objectnav/mp3d/v0/val_chair/val_one_scene.json.gz"
 
     SCREEN_SIZE = 256
     MAX_STEPS = 500
