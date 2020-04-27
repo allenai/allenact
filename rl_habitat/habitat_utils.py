00dbe7cd2148b034d6ade37783cfa74d2ee853fb
klemenkotar <klemenk@allenai.org>
2020-04-27 08:20:02 -0700

    set up experiment with all scenes, one target and 200M frames

--- rl_habitat/habitat_utils.py
+++ rl_habitat/habitat_utils.py
@@ -69,41 +69,41 @@ def construct_env_configs_mp3d(config: Config) -> List[Config]:
     num_processes = config.NUM_PROCESSES
     configs = []
 
-    small = ['rPc6DW4iMge', 'e9zR4mvMWw7', 'uNb9QFRL6hY', 'qoiz87JEwZ2', 'sKLMLpTHeUy', 's8pcmisQ38h', '759xd9YjKW5',
-             '5q7pvUzZiYa', 'XcA2TqTSSAj', 'SN83YJsR3w2', '8WUmhLawc2A', 'JeFG25nYj2p', '17DRP5sb8fy', 'Uxmj2M2itWa',
-             'D7N2EKCX4Sj', 'b8cTxDM8gDG', 'sT4fr6TAbpF', 'S9hNv5qa7GM', '82sE5b5pLXE', 'pRbA3pwrgk9', 'aayBHfsNo7d',
-             'cV4RVeZvu5T', 'i5noydFURQK', 'YmJkqBEsHnH', 'jh4fc5c5qoQ', 'VVfe2KiqLaN', '29hnd4uzFmX', 'Pm6F8kyY3z2',
-             'JF19kD82Mey', 'GdvgFV5R1Z5', 'HxpKQynjfin', 'vyrNrziPKCB']
-    med = ['V2XKFyX4ASd', 'VFuaQ6m2Qom', 'ZMojNkEp431', '5LpN3gDmAk7', 'r47D5H71a5s', 'ULsKaCPVFJR',
-           'E9uDoFAP3SH', 'kEZ7cmS4wCh', 'ac26ZMwG7aT', 'dhjEzFoUFzH', 'mJXqzFtmKg4', 'p5wJjkQkbXX', 'Vvot9Ly1tCj',
-           'EDJbREhghzL', 'VzqfbhrpDEA', '7y3sRwLe3Va']
-
-    scene_splits = [[] for _ in range(config.NUM_PROCESSES)]
-    distribute(small, scene_splits, num_gpus=8, procs_per_gpu=3, proc_offset=1, scenes_per_process=2)
-    distribute(med, scene_splits, num_gpus=8, procs_per_gpu=3, proc_offset=0, scenes_per_process=1)
-
-    # gpu0 = [['pRbA3pwrgk9', '82sE5b5pLXE', 'S9hNv5qa7GM'],
-    #         ['Uxmj2M2itWa', '17DRP5sb8fy', 'JeFG25nYj2p'],
-    #         ['5q7pvUzZiYa', '759xd9YjKW5', 's8pcmisQ38h'],
-    #         ['e9zR4mvMWw7', 'rPc6DW4iMge', 'vyrNrziPKCB']]
-    # gpu1 = [['sT4fr6TAbpF', 'b8cTxDM8gDG', 'D7N2EKCX4Sj'],
-    #         ['8WUmhLawc2A', 'SN83YJsR3w2', 'XcA2TqTSSAj'],
-    #         ['sKLMLpTHeUy', 'qoiz87JEwZ2', 'uNb9QFRL6hY'],
-    #         ['V2XKFyX4ASd', 'VFuaQ6m2Qom', 'ZMojNkEp431']]
-    # gpu2 = [['5LpN3gDmAk7', 'r47D5H71a5s', 'ULsKaCPVFJR', 'E9uDoFAP3SH'],
-    #         ['VVfe2KiqLaN', 'jh4fc5c5qoQ', 'YmJkqBEsHnH'],  # small
-    #         ['i5noydFURQK', 'cV4RVeZvu5T', 'aayBHfsNo7d']]  # small
-    # gpu3 = [['kEZ7cmS4wCh', 'ac26ZMwG7aT', 'dhjEzFoUFzH'],
-    #         ['mJXqzFtmKg4', 'p5wJjkQkbXX', 'Vvot9Ly1tCj']]
-    # gpu4 = [['EDJbREhghzL', 'VzqfbhrpDEA', '7y3sRwLe3Va'],
-    #         ['ur6pFq6Qu1A', 'PX4nDJXEHrG', 'PuKPg4mmafe']]
-    # gpu5 = [['r1Q1Z4BcV1o', 'gTV8FGcVJC9', '1pXnuDYAj8r'],
-    #         ['JF19kD82Mey', 'Pm6F8kyY3z2', '29hnd4uzFmX']]  # small
-    # gpu6 = [['VLzqgDo317F', '1LXtFkjw3qL'],
-    #         ['HxpKQynjfin', 'gZ6f7yhEvPG', 'GdvgFV5R1Z5']]  # small
-    # gpu7 = [['D7G3Y4RVNrH', 'B6ByNegPMKs']]
+    # small = ['rPc6DW4iMge', 'e9zR4mvMWw7', 'uNb9QFRL6hY', 'qoiz87JEwZ2', 'sKLMLpTHeUy', 's8pcmisQ38h', '759xd9YjKW5',
+    #          '5q7pvUzZiYa', 'XcA2TqTSSAj', 'SN83YJsR3w2', '8WUmhLawc2A', 'JeFG25nYj2p', '17DRP5sb8fy', 'Uxmj2M2itWa',
+    #          'D7N2EKCX4Sj', 'b8cTxDM8gDG', 'sT4fr6TAbpF', 'S9hNv5qa7GM', '82sE5b5pLXE', 'pRbA3pwrgk9', 'aayBHfsNo7d',
+    #          'cV4RVeZvu5T', 'i5noydFURQK', 'YmJkqBEsHnH', 'jh4fc5c5qoQ', 'VVfe2KiqLaN', '29hnd4uzFmX', 'Pm6F8kyY3z2',
+    #          'JF19kD82Mey', 'GdvgFV5R1Z5', 'HxpKQynjfin', 'vyrNrziPKCB']
+    # med = ['V2XKFyX4ASd', 'VFuaQ6m2Qom', 'ZMojNkEp431', '5LpN3gDmAk7', 'r47D5H71a5s', 'ULsKaCPVFJR',
+    #        'E9uDoFAP3SH', 'kEZ7cmS4wCh', 'ac26ZMwG7aT', 'dhjEzFoUFzH', 'mJXqzFtmKg4', 'p5wJjkQkbXX', 'Vvot9Ly1tCj',
+    #        'EDJbREhghzL', 'VzqfbhrpDEA', '7y3sRwLe3Va']
     #
-    # scene_splits = gpu0 + gpu1 + gpu2 + gpu3 + gpu4 + gpu5 + gpu6 + gpu7
+    # scene_splits = [[] for _ in range(config.NUM_PROCESSES)]
+    # distribute(small, scene_splits, num_gpus=8, procs_per_gpu=3, proc_offset=1, scenes_per_process=2)
+    # distribute(med, scene_splits, num_gpus=8, procs_per_gpu=3, proc_offset=0, scenes_per_process=1)
+
+    gpu0 = [['pRbA3pwrgk9', '82sE5b5pLXE', 'S9hNv5qa7GM'],
+            ['Uxmj2M2itWa', '17DRP5sb8fy', 'JeFG25nYj2p'],
+            ['5q7pvUzZiYa', '759xd9YjKW5', 's8pcmisQ38h'],
+            ['e9zR4mvMWw7', 'rPc6DW4iMge', 'vyrNrziPKCB']]
+    gpu1 = [['sT4fr6TAbpF', 'b8cTxDM8gDG', 'D7N2EKCX4Sj'],
+            ['8WUmhLawc2A', 'SN83YJsR3w2', 'XcA2TqTSSAj'],
+            ['sKLMLpTHeUy', 'qoiz87JEwZ2', 'uNb9QFRL6hY'],
+            ['V2XKFyX4ASd', 'VFuaQ6m2Qom', 'ZMojNkEp431']]
+    gpu2 = [['5LpN3gDmAk7', 'r47D5H71a5s', 'ULsKaCPVFJR', 'E9uDoFAP3SH'],
+            ['VVfe2KiqLaN', 'jh4fc5c5qoQ', 'YmJkqBEsHnH'],  # small
+            ['i5noydFURQK', 'cV4RVeZvu5T', 'aayBHfsNo7d']]  # small
+    gpu3 = [['kEZ7cmS4wCh', 'ac26ZMwG7aT', 'dhjEzFoUFzH'],
+            ['mJXqzFtmKg4', 'p5wJjkQkbXX', 'Vvot9Ly1tCj']]
+    gpu4 = [['EDJbREhghzL', 'VzqfbhrpDEA', '7y3sRwLe3Va'],
+            ['ur6pFq6Qu1A', 'PX4nDJXEHrG', 'PuKPg4mmafe']]
+    gpu5 = [['r1Q1Z4BcV1o', 'gTV8FGcVJC9', '1pXnuDYAj8r'],
+            ['JF19kD82Mey', 'Pm6F8kyY3z2', '29hnd4uzFmX']]  # small
+    gpu6 = [['VLzqgDo317F', '1LXtFkjw3qL'],
+            ['HxpKQynjfin', 'gZ6f7yhEvPG', 'GdvgFV5R1Z5']]  # small
+    gpu7 = [['D7G3Y4RVNrH', 'B6ByNegPMKs']]
+
+    scene_splits = gpu0 + gpu1 + gpu2 + gpu3 + gpu4 + gpu5 + gpu6 + gpu7
 
     for i in range(num_processes):
 
