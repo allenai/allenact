3a07b2c83f14bbc4ebb826a99ddde58d32bf4b6e
klemenkotar <klemenk@allenai.org>
2020-05-03 02:24:37 -0700

    Switched to using only 20 procs for single instance of target per scene experiment

--- rl_habitat/habitat_utils.py
+++ rl_habitat/habitat_utils.py
@@ -30,9 +30,7 @@ def construct_env_configs(config: Config) -> List[Config]:
 
     scenes = ['29hnd4uzFmX', 'i5noydFURQK', 'cV4RVeZvu5T', '82sE5b5pLXE', 'JeFG25nYj2p', '8WUmhLawc2A', 'uNb9QFRL6hY',
               'rPc6DW4iMge', 'vyrNrziPKCB', 'VFuaQ6m2Qom', '29hnd4uzFmX', 'i5noydFURQK', 'cV4RVeZvu5T', '82sE5b5pLXE',
-              'JeFG25nYj2p', '8WUmhLawc2A', 'uNb9QFRL6hY', 'rPc6DW4iMge', 'vyrNrziPKCB', 'VFuaQ6m2Qom', '29hnd4uzFmX',
-              'i5noydFURQK', 'cV4RVeZvu5T', '82sE5b5pLXE', 'JeFG25nYj2p', '8WUmhLawc2A', 'uNb9QFRL6hY', 'rPc6DW4iMge',
-              'vyrNrziPKCB', 'VFuaQ6m2Qom']
+              'JeFG25nYj2p', '8WUmhLawc2A', 'uNb9QFRL6hY', 'rPc6DW4iMge', 'vyrNrziPKCB', 'VFuaQ6m2Qom']
 
     if len(scenes) > 0:
         # random.shuffle(scenes)
