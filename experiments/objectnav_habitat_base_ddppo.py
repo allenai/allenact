2b9ee44b649688fe76a3fadeb685ecf8f32e52ff
klemenkotar <klemenk@allenai.org>
2020-05-08 08:42:45 -0700

    Added debug logging

--- experiments/objectnav_habitat_base_ddppo.py
+++ experiments/objectnav_habitat_base_ddppo.py
@@ -76,7 +76,7 @@ class ObjectNavHabitatDDPPOBaseExperimentConfig(ExperimentConfig):
         num_mini_batch = 1
         update_repeats = 4
         num_steps = 128
-        save_interval = 5000000
+        save_interval = 100 # 5000000
         log_interval = 10000
         gamma = 0.99
         use_gae = True
