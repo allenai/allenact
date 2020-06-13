60b69a1cc7340a8bca3072d36ddaa02a69235926
klemenkotar <klemenk@allenai.org>
2020-06-12 23:34:27 -0700

    Reverted back to doing val every 5M steps

--- experiments/pointnav_robothor_depth_ddppo.py
+++ experiments/pointnav_robothor_depth_ddppo.py
@@ -102,7 +102,7 @@ class ObjectNavRoboThorRGBPPOExperimentConfig(ExperimentConfig):
         num_mini_batch = 1
         update_repeats = 3
         num_steps = 30
-        save_interval = 10000
+        save_interval = 5000000
         log_interval = 10000
         gamma = 0.99
         use_gae = True
