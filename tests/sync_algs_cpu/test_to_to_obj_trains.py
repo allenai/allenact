c4d2eeb6c316f5e9249735142d0d18b7d47d0903
Jordi Salvador <jordis@allenai.org>
2020-08-05 14:48:05 -0700

    Fix test

--- tests/sync_algs_cpu/test_to_to_obj_trains.py
+++ tests/sync_algs_cpu/test_to_to_obj_trains.py
@@ -1,8 +1,6 @@
 import math
 import os
 
-import py
-
 from constants import ABS_PATH_OF_TOP_LEVEL_DIR
 from onpolicy_sync.runner import OnPolicyRunner
 from projects.babyai_baselines.experiments.go_to_obj.ppo import (
@@ -11,15 +9,10 @@ from projects.babyai_baselines.experiments.go_to_obj.ppo import (
 
 
 class TestGoToObjTrains(object):
-    def test_ppo_trains(
-        self, tmpdir=py.path.local(os.path.join(ABS_PATH_OF_TOP_LEVEL_DIR, "tests/tmp"))
-    ):
+    def test_ppo_trains(self, tmpdir):
         cfg = PPOBabyAIGoToObjExperimentConfig()
 
-        tmpdir = tmpdir.join("experiment_output")
-        if not tmpdir.exists():
-            tmpdir.mkdir()
-        output_dir: str = tmpdir.dirname
+        output_dir: str = tmpdir.mkdir("experiment_output").dirname
 
         train_runner = OnPolicyRunner(
             config=cfg,
