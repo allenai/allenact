3868c1d8ca8d11aef253747a91b846eb42441bcf
Aniruddha Kembhavi <anik@allenai.org>
2020-08-27 17:19:31 -0700

    Add API back into docs

--- scripts/build_docs.py
+++ scripts/build_docs.py
@@ -229,8 +229,7 @@ if __name__ == "__main__":
             nav_obj = obj
             break
 
-    # Uncomment this line when you want to build and publish API docs
-    # nav_obj[docs_key] = nav_entries
+    nav_obj[docs_key] = nav_entries
 
     with open(yaml_path, "w") as f:
         yaml.dump(mkdocs_yaml, f)
