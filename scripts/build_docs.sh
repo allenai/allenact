ff2a7a004b468339e33640d341a10cdaf3a6690e
lucaw <lucaw@allenai.org>
2020-08-28 17:10:14 -0700

    Improving documentation about generating documentation.

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Add allenact to the python path
+export PYTHONPATH=$PYTHONPATH:$PWD
+
 # Alter the relative path of the README image for the docs.
 #sed -i '1s/docs/./' docs/README.md
 python scripts/build_docs.py
