4159e48c83e58fae0e4ed394f53cf65aaced01d7
lucaw <lucaw@allenai.org>
2020-09-09 11:11:12 -0700

    Updating the download_navigation_datasets.sh script to automatically create a debug dataset for ithor objectnav.

--- datasets/download_navigation_datasets.sh
+++ datasets/download_navigation_datasets.sh
@@ -35,6 +35,9 @@ elif [ "$1" = "ithor-objectnav" ]
 then
     echo "Downloading iTHOR ObjectNav Dataset ..."
     install_dataset "$1"
+    cd "$(pwd)"/..
+    echo "Generating iTHOR ObjectNav Debug Dataset ..."
+    PYTHONPATH=. python ./plugins/ithor_plugin/scripts/make_objectnav_debug_dataset.py
 
 else
     echo "Failed: Usage download_navigation_datasets.sh robothor-pointnav | robothor-objectnav | ithor-pointnav | ithor-objectnav"
