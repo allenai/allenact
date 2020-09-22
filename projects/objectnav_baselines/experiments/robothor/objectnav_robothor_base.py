71c7bc915cbc907dfc2bcf171e120aac7d6a6fd5
lucaw <lucaw@allenai.org>
2020-09-22 16:44:15 -0700

    RoboTHOR ObjectNav DAgger config.

--- projects/objectnav_baselines/experiments/robothor/objectnav_robothor_base.py
+++ projects/objectnav_baselines/experiments/robothor/objectnav_robothor_base.py
@@ -96,7 +96,7 @@ class ObjectNavRoboThorBaseConfig(ObjectNavBaseConfig):
             sampler_devices = self.SAMPLER_GPU_IDS
             render_video = False
         elif mode == "valid":
-            nprocesses = 15 if torch.cuda.is_available() else 0
+            nprocesses = 5 if torch.cuda.is_available() else 0
             gpu_ids = [] if not torch.cuda.is_available() else self.VALID_GPU_IDS
             render_video = False
         elif mode == "test":
