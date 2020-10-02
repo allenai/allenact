e70d26e59c690271dbb44bbd0e4fd6a6148ab575
Klemen Kotar <klemen@kotar.us>
2020-10-02 12:24:34 -0700

    Lowered dybamic cache resolution

--- plugins/robothor_plugin/robothor_environment.py
+++ plugins/robothor_plugin/robothor_environment.py
@@ -48,7 +48,7 @@ class RoboThorEnvironment:
         self.known_good_locations: Dict[str, Any] = {
             self.scene_name: copy.deepcopy(self.currently_reachable_points)
         }
-        self.distance_cache = DynamicDistanceCache(rounding=2)
+        self.distance_cache = DynamicDistanceCache(rounding=1)
         assert len(self.known_good_locations[self.scene_name]) > 10
 
     def initialize_grid_dimensions(
