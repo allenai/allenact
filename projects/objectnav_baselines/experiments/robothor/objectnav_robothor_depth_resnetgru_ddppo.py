fbcc81e22f19f5b43b2124c05f8a6cf888893b33
Klemen Kotar <klemen@kotar.us>
2020-10-02 16:00:39 -0700

    Reduced number of epoch updates back to 3

--- projects/objectnav_baselines/experiments/robothor/objectnav_robothor_depth_resnetgru_ddppo.py
+++ projects/objectnav_baselines/experiments/robothor/objectnav_robothor_depth_resnetgru_ddppo.py
@@ -67,7 +67,7 @@ class ObjectNavRoboThorRGBPPOExperimentConfig(ObjectNavRoboThorBaseConfig):
         ppo_steps = int(300000000)
         lr = 3e-4
         num_mini_batch = 1
-        update_repeats = 4
+        update_repeats = 3
         num_steps = 30
         save_interval = 5000000
         log_interval = 10000
