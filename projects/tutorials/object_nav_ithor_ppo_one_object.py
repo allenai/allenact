a9dbe1a8533bf78ec0ecf8dbe64288781b066084
Eric Kolve <erick@allenai.org>
2020-12-17 15:22:36 -0800

    reverting changes that renamed player_screen_- to width/height

--- projects/tutorials/object_nav_ithor_ppo_one_object.py
+++ projects/tutorials/object_nav_ithor_ppo_one_object.py
@@ -45,8 +45,8 @@ class ObjectNavThorPPOExperimentConfig(ExperimentConfig):
     ]
 
     ENV_ARGS = {
-        "height": SCREEN_SIZE,
-        "width": SCREEN_SIZE,
+        "player_scren_height": SCREEN_SIZE,
+        "player_scren_width": SCREEN_SIZE,
         "quality": "Very Low",
     }
 
