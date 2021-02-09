2159439006e273bc7863d8ca06267527233ba00e
Jordi Salvador <jordis@allenai.org>
2021-02-09 10:12:20 -0800

    Default version None (unless pip installed from MyPI)

--- allenact/__init__.py
+++ allenact/__init__.py
@@ -1,15 +1,4 @@
-import os
-
-from pathlib import Path
-
-__version__ = None
-
 try:
     from allenact._version import __version__
 except ModuleNotFoundError:
-    try:
-        with open(os.path.join(os.path.dirname(Path(__file__)), "..", ".VERSION")) as f:
-            __version__ = f.read().strip()
-        pass
-    except ModuleNotFoundError:
-        pass
+    __version__ = None
