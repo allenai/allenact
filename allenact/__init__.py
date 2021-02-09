fb38a3c18ab0a0db11a499106790eb91330a90dc
Jordi Salvador <jordis@allenai.org>
2021-02-08 18:33:55 -0800

    Added version to allenact_plugins, allenact

---
+++ allenact/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+from pathlib import Path
+
+__version__ = None
+
+try:
+    from allenact._version import __version__
+except ModuleNotFoundError:
+    try:
+        with open(os.path.join(os.path.dirname(Path(__file__)), "..", ".VERSION")) as f:
+            __version__ = f.read().strip()
+        pass
+    except ModuleNotFoundError:
+        pass
