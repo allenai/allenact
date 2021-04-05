0de84bb68555d9922556b2ce989cf3d5bd48e9c3
Luca Weihs <lucaw@allenai.org>
2021-04-05 10:12:43 -0700

    Updating the startx.py script so it doesn't autoformat breaking quotes.

--- scripts/startx.py
+++ scripts/startx.py
@@ -1,11 +1,16 @@
-import atexit
-import os
-import platform
-import re
-import shlex
 import subprocess
+import shlex
+import re
+import atexit
+import platform
 import tempfile
+import threading
+import os
+import sys
 
+# Turning off automatic black formatting for this script as it breaks quotes.
+
+# fmt: off
 
 def pci_records():
     records = []
@@ -20,7 +25,6 @@ def pci_records():
             record[key.split(":")[0]] = value
 
     return records
-
 
 def generate_xorg_conf(devices):
     xorg_conf = []
@@ -55,17 +59,12 @@ EndSection
     for i, bus_id in enumerate(devices):
         xorg_conf.append(device_section.format(device_id=i, bus_id=bus_id))
         xorg_conf.append(screen_section.format(device_id=i, screen_id=i))
-        screen_records.append(
-            'Screen {screen_id} "Screen{screen_id}" 0 0'.format(screen_id=i)
-        )
+        screen_records.append('Screen {screen_id} "Screen{screen_id}" 0 0'.format(screen_id=i))
+    
+    xorg_conf.append(server_layout_section.format(screen_records="\n    ".join(screen_records)))
 
-    xorg_conf.append(
-        server_layout_section.format(screen_records="\n    ".join(screen_records))
-    )
-
-    output = "\n".join(xorg_conf)
+    output =  "\n".join(xorg_conf)
     return output
-
 
 def startx(display=0):
     if platform.system() != "Linux":
@@ -73,32 +72,27 @@ def startx(display=0):
 
     devices = []
     for r in pci_records():
-        if r.get("Vendor", "") == "NVIDIA Corporation" and r["Class"] in [
-            "VGA compatible controller",
-            "3D controller",
-        ]:
-            bus_id = "PCI:" + ":".join(
-                map(lambda x: str(int(x, 16)), re.split(r"[:\.]", r["Slot"]))
-            )
+        if r.get("Vendor", "") == "NVIDIA Corporation"\
+                and r["Class"] in ["VGA compatible controller", "3D controller"]:
+            bus_id = "PCI:" + ":".join(map(lambda x: str(int(x, 16)), re.split(r"[:\.]", r["Slot"])))
             devices.append(bus_id)
 
     if not devices:
         raise Exception("no nvidia cards found")
 
-    fd, path = tempfile.mkstemp()
     try:
+        fd, path = tempfile.mkstemp()
         with open(path, "w") as f:
             f.write(generate_xorg_conf(devices))
-        command = shlex.split(
-            "Xorg -noreset +extension GLX +extension RANDR +extension RENDER -config %s :%s"
-            % (path, display)
-        )
+        command = shlex.split("Xorg -noreset +extension GLX +extension RANDR +extension RENDER -config %s :%s" % (path, display))
         proc = subprocess.Popen(command)
         atexit.register(lambda: proc.poll() is None and proc.kill())
         proc.wait()
-    finally:
+    finally: 
         os.close(fd)
         os.unlink(path)
 
+# fmt: on
 
-startx()
+if __name__ == "__main__":
+    startx()
