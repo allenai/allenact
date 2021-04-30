8950ae556fa14913102290bb61491cb590f68a33
lucaw <lucaw@allenai.org>
2021-04-30 15:46:35 -0700

    Removing  as a required task method.

--- allenact_plugins/habitat_plugin/habitat_tasks.py
+++ allenact_plugins/habitat_plugin/habitat_tasks.py
@@ -281,6 +281,9 @@ class ObjectNavTask(HabitatTask):
     def class_action_names(cls, **kwargs) -> Tuple[str, ...]:
         return cls._actions
 
+    def action_names(cls, **kwargs) -> Tuple[str, ...]:
+        return cls._actions
+
     def close(self) -> None:
         self.env.stop()
 
