4c083629b9529aef0c8c8635af427d4a06c45689
Zichen "Charles" Zhang <52727818+zcczhang@users.noreply.github.com>
2021-08-19 16:03:40 -0700

    Update gym_base.py

--- projects/gym_baselines/experiments/gym_base.py
+++ projects/gym_baselines/experiments/gym_base.py
@@ -1,37 +1,14 @@
-import abc
 from abc import ABC
 from typing import Dict, Sequence, Optional, List, Any, cast
 
-import gym
-import torch.nn as nn
-import torch.optim as optim
-from torch.optim.lr_scheduler import LambdaLR
-
-from allenact.algorithms.onpolicy_sync.losses.ppo import PPO
-
-from allenact.algorithms.onpolicy_sync.losses.a2cacktr import A2C
-from allenact.algorithms.onpolicy_sync.losses.a2cacktr import A2CConfig
-
-from allenact.base_abstractions.experiment_config import ExperimentConfig, TaskSampler
-from allenact.base_abstractions.sensor import Sensor, SensorSuite
-from allenact_plugins.gym_plugin.gym_models import MemorylessActorCritic
-from allenact_plugins.gym_plugin.gym_sensors import GymMuJoCoSensor
-
-from allenact_plugins.gym_plugin.gym_tasks import GymTaskSampler
-from allenact.utils.experiment_utils import (
-    TrainingPipeline,
-    Builder,
-    PipelineStage,
-    LinearDecay,
-)
-from allenact.utils.viz_utils import VizSuite, AgentViewViz
+from allenact.base_abstractions.experiment_config import ExperimentConfig
+from allenact.base_abstractions.sensor import Sensor
 
 
-class GymBaseConfig(ExperimentConfig):
+class GymBaseConfig(ExperimentConfig, ABC):
 
     SENSORS: Optional[Sequence[Sensor]] = None
 
-    # @abc.abstractmethod
     def _get_sampler_args(
         self, process_ind: int, mode: str, seeds: List[int]
     ) -> Dict[str, Any]:
