c5c86500457b4a17a973e3b466c6054381e43ed5
lucaw <lucaw@allenai.org>
2021-09-09 14:02:01 -0700

    Minor renaming.

--- allenact/algorithms/onpolicy_sync/runner.py
+++ allenact/algorithms/onpolicy_sync/runner.py
@@ -407,8 +407,7 @@ class OnPolicyRunner(object):
                 else model_hash,
             )
             train: BaseProcess = self.mp_ctx.Process(
-                target=self.train_loop,
-                kwargs=training_kwargs,
+                target=self.train_loop, kwargs=training_kwargs,
             )
             try:
                 train.start()
@@ -421,8 +420,7 @@ class OnPolicyRunner(object):
                     model_hash = md5_hash_of_state_dict(initial_model_state_dict)
                     training_kwargs["initial_model_state_dict"] = model_hash
                     train = self.mp_ctx.Process(
-                        target=self.train_loop,
-                        kwargs=training_kwargs,
+                        target=self.train_loop, kwargs=training_kwargs,
                     )
                     train.start()
                 else:
@@ -677,14 +675,10 @@ class OnPolicyRunner(object):
                 # name collisions.
                 k = -1
                 while True:
-                    prefix = "" if k == -1 else "namecollision{}__".format(k)
+                    prefix = "" if k == -1 else f"namecollision{k}__"
                     k += 1
                     dst_path = os.path.join(
-                        base_dir,
-                        "{}{}".format(
-                            prefix,
-                            os.path.basename(src_path),
-                        ),
+                        base_dir, f"{prefix}{os.path.basename(src_path)}",
                     )
                     if not os.path.exists(dst_path):
                         os.makedirs(os.path.dirname(dst_path), exist_ok=True)
