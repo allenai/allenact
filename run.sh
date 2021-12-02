4dcf7f1dad3859ccd96543e778be0fe9fabaa205
twni2016 <twni2016@gmail.com>
2021-12-02 14:22:48 -0800

    Merge remote-tracking branch 'origin/manipulathor_plugin' into disturb-free-module

---
+++ run.sh
@@ -0,0 +1,6 @@
+# python main.py projects/objectnav_baselines/experiments/robothor/objectnav_robothor_rgb_rawgru_ddppo.py \
+#     -o experiment_output/objectnav/ --seed 302
+# python main.py projects/pointnav_baselines/experiments/robothor/pointnav_robothor_rgb_simpleconvgru_ddppo.py\
+#     -o experiment_output/pointnav/ --seed 302
+python main.py projects/manipulathor_disturb_free/armpointnav_baselines/experiments/ithor/armpointnav_depth.py \
+    -o experiment_output/armpointnav/ --seed 302
