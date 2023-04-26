c4ff582375fc102561b785cd1facdcb1fba97413
lucaw <lucaw@allenai.org>
2023-04-26 10:00:10 -0700

    Catching more issues when passing model weights to the engine from the runner.

--- allenact/algorithms/onpolicy_sync/runner.py
+++ allenact/algorithms/onpolicy_sync/runner.py
@@ -99,7 +99,7 @@ class OnPolicyRunner(object):
         self.config = config
         self.output_dir = output_dir
         self.loaded_config_src_files = loaded_config_src_files
-        self.seed = seed if seed is not None else random.randint(0, 2 ** 31 - 1)
+        self.seed = seed if seed is not None else random.randint(0, 2**31 - 1)
         self.deterministic_cudnn = deterministic_cudnn
         self.distributed_preemption_threshold = distributed_preemption_threshold
         if multiprocessing_start_method == "default":
@@ -564,20 +564,27 @@ class OnPolicyRunner(object):
                 try_restart_after_task_error=try_restart_after_task_error,
             )
             train: BaseProcess = self.mp_ctx.Process(
-                target=self.train_loop, kwargs=training_kwargs,
+                target=self.train_loop,
+                kwargs=training_kwargs,
             )
             try:
                 train.start()
-            except ValueError as e:
+            except (ValueError, OSError, ConnectionRefusedError, EOFError) as e:
                 # If the `initial_model_state_dict` is too large we sometimes
                 # run into errors passing it with multiprocessing. In such cases
                 # we instead hash the state_dict and confirm, in each engine worker, that
                 # this hash equals the model the engine worker instantiates.
-                if e.args[0] == "too many fds":
+                if (
+                    (isinstance(e, ValueError) and e.args[0] == "too many fds")
+                    or (isinstance(e, OSError) and e.errno == 22)
+                    or (isinstance(e, ConnectionRefusedError) and e.errno == 111)
+                    or isinstance(e, EOFError)
+                ):
                     model_hash = md5_hash_of_state_dict(initial_model_state_dict)
                     training_kwargs["initial_model_state_dict"] = model_hash
                     train = self.mp_ctx.Process(
-                        target=self.train_loop, kwargs=training_kwargs,
+                        target=self.train_loop,
+                        kwargs=training_kwargs,
                     )
                     train.start()
                 else:
@@ -781,9 +788,17 @@ class OnPolicyRunner(object):
             start_time_str or self.local_start_time_str,
         ]
         if self.save_dir_fmt == SaveDirFormat.NESTED:
-            folder = os.path.join(self.output_dir, *path_parts, "checkpoints",)
+            folder = os.path.join(
+                self.output_dir,
+                *path_parts,
+                "checkpoints",
+            )
         elif self.save_dir_fmt == SaveDirFormat.FLAT:
-            folder = os.path.join(self.output_dir, "checkpoints", *path_parts,)
+            folder = os.path.join(
+                self.output_dir,
+                "checkpoints",
+                *path_parts,
+            )
         else:
             raise NotImplementedError
         if create_if_none:
@@ -826,7 +841,10 @@ class OnPolicyRunner(object):
     def metric_path(self, start_time_str: str) -> str:
         if self.save_dir_fmt == SaveDirFormat.NESTED:
             return os.path.join(
-                self.output_dir, "test", self.config.tag(), start_time_str,
+                self.output_dir,
+                "test",
+                self.config.tag(),
+                start_time_str,
             )
         elif self.save_dir_fmt == SaveDirFormat.FLAT:
             return os.path.join(
@@ -848,9 +866,17 @@ class OnPolicyRunner(object):
             self.local_start_time_str,
         ]
         if self.save_dir_fmt == SaveDirFormat.NESTED:
-            base_dir = os.path.join(self.output_dir, *path_parts, "used_configs",)
+            base_dir = os.path.join(
+                self.output_dir,
+                *path_parts,
+                "used_configs",
+            )
         elif self.save_dir_fmt == SaveDirFormat.FLAT:
-            base_dir = os.path.join(self.output_dir, "used_configs", *path_parts,)
+            base_dir = os.path.join(
+                self.output_dir,
+                "used_configs",
+                *path_parts,
+            )
         else:
             raise NotImplementedError
         os.makedirs(base_dir, exist_ok=True)
@@ -893,7 +919,8 @@ class OnPolicyRunner(object):
                     prefix = "" if k == -1 else f"namecollision{k}__"
                     k += 1
                     dst_path = os.path.join(
-                        base_dir, f"{prefix}{os.path.basename(src_path)}",
+                        base_dir,
+                        f"{prefix}{os.path.basename(src_path)}",
                     )
                     if not os.path.exists(dst_path):
                         os.makedirs(os.path.dirname(dst_path), exist_ok=True)
@@ -1033,13 +1060,16 @@ class OnPolicyRunner(object):
                     )
 
                 info_means = update_keys_misc(
-                    info_tracker.means(), stage_component_uuid,
+                    info_tracker.means(),
+                    stage_component_uuid,
                 )
                 info_counts = update_keys_misc(
-                    info_tracker.counts(), stage_component_uuid,
+                    info_tracker.counts(),
+                    stage_component_uuid,
                 )
                 metrics_and_info_tracker.add_scalars(
-                    scalars=info_means, n=info_counts,
+                    scalars=info_means,
+                    n=info_counts,
                 )
 
                 total_exp_for_storage = pkg.storage_uuid_to_total_experiences[
@@ -1122,7 +1152,8 @@ class OnPolicyRunner(object):
                         storage_uuid
                     ]:
                         approx_eps_key = update_keys_misc(
-                            f"approx_eps", stage_component_uuid,
+                            f"approx_eps",
+                            stage_component_uuid,
                         )
                         callback_metric_means[approx_eps_key] = eps
                         scalar_name_to_total_experiences_key[
@@ -1131,7 +1162,9 @@ class OnPolicyRunner(object):
 
                         if log_writer is not None:
                             log_writer.add_scalar(
-                                approx_eps_key, eps, cur_total_exp,
+                                approx_eps_key,
+                                eps,
+                                cur_total_exp,
                             )
 
         metrics_and_info_means_with_metrics_dicts_list = copy.deepcopy(
